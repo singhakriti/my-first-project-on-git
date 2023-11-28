72bb95efe76a987c5ecfc1469b8f14fff4040686
singhakriti <151597888+singhakriti@users.noreply.github.com>
2023-11-28 09:15:31 +0530

    Add files via upload

---
+++ ExplicitTypeCasting.java
@@ -0,0 +1,14 @@
+
+public class ExplicitTypeCasting {
+
+	public static void main(String[] args) {
+		double num = 10.99;
+	    System.out.println("The double value: " + num);
+
+	    // convert into int type
+	    int data = (int)num;
+	    System.out.println("The integer value: " + data);
+
+	}
+
+}
---
+++ ImplicitTypeCasting.java
@@ -0,0 +1,14 @@
+
+public class ImplicitTypeCasting {
+
+	public static void main(String[] args) {
+		 int num = 10;
+		    System.out.println("The integer value: " + num);
+
+		    // convert into double type
+		    double data = num;
+		    System.out.println("The double value: " + data);
+
+	}
+
+}
